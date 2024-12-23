74b642847c79bdcd388f043d5b193e5c93228213
Alex Long <alo72@uclive.ac.nz>
2024-12-23 20:17:09 +1300

    fix footer not loading

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Box, Typography, Link } from "@mui/material";
+import {Box, Typography, Link, MenuItem} from "@mui/material";
+function Footer() {
 
-const Footer = () => {
+    const testLoad = () => {
+        console.log("footer loaded");
+    }
+
     return (
         <Box
             component="footer"
@@ -15,13 +19,22 @@ const Footer = () => {
                         : theme.palette.grey[800],
             }}
         >
+
             <Typography variant="body1" align="center">
                 © {new Date().getFullYear()} Your Company Name
             </Typography>
             <Typography variant="body2" align="center">
                 Built with <Link href="https://mui.com/" underline="hover">MUI</Link>
             </Typography>
+            <MenuItem
+                onClick={() => {
+                    testLoad();
+                }}
+            >
+                Logout (test)
+            </MenuItem>
         </Box>
+
     );
 };
 
