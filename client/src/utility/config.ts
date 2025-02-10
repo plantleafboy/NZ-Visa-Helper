d25dc4358847786003965174be2e5144f7a0573d
Alex Long <alo72@uclive.ac.nz>
2025-02-10 18:41:25 +1300

    change config.ts url again to 'https://squid-app-2inh2.ondigitalocean.app/nz-visa-helper-client2/:5000'

--- client/src/utility/config.ts
+++ client/src/utility/config.ts
@@ -1,4 +1,4 @@
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? 'https://squid-app-2inh2.ondigitalocean.app/:5000'
+    ? 'https://squid-app-2inh2.ondigitalocean.app/nz-visa-helper-client2/:5000'
     : `http://localhost:4941`;
 export {BASE_URL}
