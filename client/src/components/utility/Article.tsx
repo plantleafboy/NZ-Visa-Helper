5fc82aad6a0b82f6322e8b515de909feecab8d54
Alex Long <alo72@uclive.ac.nz>
2025-06-03 15:26:07 +1200

    finish Article component structure and style

--- client/src/components/utility/Article.tsx
+++ client/src/components/utility/Article.tsx
@@ -1,4 +1,4 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import React, {useEffect, useState} from "react";
 
 interface ArticleProps {
@@ -9,7 +9,57 @@ interface ArticleProps {
 
 const Article = (props: ArticleProps) => {
     return (
-        <Box>{props.content}</Box>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                maxHeight: '900px',
+            }}
+        >
+            {/* Header Section */}
+            <Box
+                sx={{
+                    flexShrink: 0,
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 1,
+                    backgroundColor: 'background.paper',
+                    borderBottom: '1px solid',
+                    borderColor: 'divider',
+                }}
+            >
+                <Box
+                    component="img"
+                    src={props.image}
+                    alt={props.title}
+                    sx={{
+                        width: '100%',
+                        height: 250,
+                        objectFit: 'cover',
+                    }}
+                />
+                <Typography
+                    variant="h5"
+                    fontWeight="bold"
+                    sx={{ px: 3, py: 2 }}
+                >
+                    {props.title}
+                </Typography>
+            </Box>
+
+            {/* Scrollable Content */}
+            <Box
+                sx={{
+                    overflowY: 'auto',
+                    px: 3,
+                    py: 2,
+                }}
+            >
+                <Typography variant="body1" color="text.secondary" whiteSpace="pre-line">
+                    {props.content}
+                </Typography>
+            </Box>
+        </Box>
     )
 }
 
