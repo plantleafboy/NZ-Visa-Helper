92a140098edf5be2d9dda41b337e52d390148aa9
Alex Long <alo72@uclive.ac.nz>
2026-02-22 00:40:15 +1300

    shift string to ENV

--- client/src/utility/config.ts
+++ client/src/utility/config.ts
@@ -1,4 +1,4 @@
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? 'https://squid-app-2inh2.ondigitalocean.app/nz-visa-helper-client2/:5000'
+    ? process.env.PROD_URL
     : `http://localhost:4941`;
 export {BASE_URL}
